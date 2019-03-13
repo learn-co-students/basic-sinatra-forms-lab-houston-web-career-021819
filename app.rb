861e52794122bb3174c12f53e447e070ecc0b4b2
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-03-13 00:05:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+       # @new_team = params[:new_team]#having this line of code gave 3 errors for quite a long time.
+        erb :newteam
+    end
 
-
+    get '/team' do 
+        erb :team
+    end
+    post '/team' do 
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @pf = params[:pf]
+        @sf = params[:sf]
+        @c = params[:c]
+       erb :team
+    end
 end
